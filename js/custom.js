04357c03bebbc91c3fc5b2131e84f73773e10031
sofiakoyama <63819185+sofiakoyama@users.noreply.github.com>
2020-10-23 17:38:12 -0700

    uploaded javascript and css files

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.fade').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+  });    
+});
